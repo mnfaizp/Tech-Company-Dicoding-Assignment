362b8741b56841fe5e78d01a0e41a203c3c70633
Znth3 <zynthkas@gmail.com>
2021-03-23 12:48:04 +0700

    finish

--- app/src/main/java/com/dicoding/techcompany/MainActivity.kt
+++ app/src/main/java/com/dicoding/techcompany/MainActivity.kt
@@ -1,34 +1,40 @@
 package com.dicoding.techcompany
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.dicoding.techcompany.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var rvCompanies: RecyclerView
+
+    private lateinit var binding: ActivityMainBinding
+
     private var list: ArrayList<Company> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.title = "10 Big Tech Company"
 
-        rvCompanies = findViewById(R.id.rv_companies)
-        rvCompanies.setHasFixedSize(true)
+        binding.rvCompanies.setHasFixedSize(true)
 
         list.addAll(CompaniesData.listData)
         showRecyclerCardView()
     }
 
     private fun showRecyclerCardView() {
-        rvCompanies.layoutManager = LinearLayoutManager(this)
+        binding.rvCompanies.layoutManager = LinearLayoutManager(this)
         val cardViewCompanyAdapter = CardViewCompanyAdapter(list)
-        rvCompanies.adapter = cardViewCompanyAdapter
+        binding.rvCompanies.adapter = cardViewCompanyAdapter
 
         cardViewCompanyAdapter.setOnItemClickCallback(object : CardViewCompanyAdapter.OnItemClickCallback {
             override fun onItemClicked(data: Company) {
@@ -39,13 +45,41 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
-        return super.onCreateOptionsMenu(menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu?.findItem(R.id.search)?.actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val moveToAboutMe = Intent(this, AboutMeActivity::class.java)
-        startActivity(moveToAboutMe)
-        return super.onOptionsItemSelected(item)
+        when (item.itemId) {
+            R.id.about_me -> {
+                val moveToAboutMe = Intent(this, AboutMeActivity::class.java)
+                startActivity(moveToAboutMe)
+                return true
+            }
+
+            R.id.announcement -> {
+                Toast.makeText(this, "This is Announcement", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            else -> return false
+        }
+
     }
 
     private fun showDetail(company: Company) {
